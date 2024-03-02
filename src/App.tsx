1bc71f7fd45881c1d07afc639d3f92b50587014d
N-Ashiono <nigelrono@yahoo.co.uk>
2024-03-02 03:31:00 +0000

    enhanc(Answer): Added more styles to the answer

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,11 @@ import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isRevealed, setIsRevealed] = useState<boolean>(false)
+
+  function handleReveal() {
+    setIsRevealed(true)
+  }
 
   return (
     <>
@@ -15,7 +19,9 @@ function App() {
 
         <div className="main">
           <div className="question">Why did the cat live to 100?</div>
-          <div className="answer">Because it was a god among beings.</div>
+          <div className={isRevealed ? "answer" : "hidden"}>
+            Because it was a god among beings.
+          </div>
           <div className="questionNav">
             <button className="arrows">
               <FontAwesomeIcon icon={faArrowLeft} />
@@ -23,7 +29,9 @@ function App() {
             <button className="arrows">
               <FontAwesomeIcon icon={faArrowRight} />
             </button>
-            <button className="reveal">Reveal Answer</button>
+            <button className="reveal" onClick={handleReveal}>
+              Reveal Answer
+            </button>
           </div>
         </div>
 
